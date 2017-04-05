ea74c6272c3b415e5b82528beca573e07c1e573c
Jose Giraldo <Jose.X.Giraldo.-ND@disney.com>
2017-04-05 14:48:29 -0500

    add CORS configuration

--- index.js
+++ index.js
@@ -16,11 +16,13 @@ var express     = require('express'),
 var config     = require('./src/config/database'),
     routes     = require('./src/routes/index'),
     secure     = require('./src/middlewares/security');
-    
-    corsOptions = {
-        origin: 'http://example.com',
-        optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-    }
+
+  
+var corsOptions = {
+    origin: 'http://example.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 
 mongoose.Promise = require('bluebird');
 
@@ -37,6 +39,17 @@ var globalBruteforce = new secure();
 app.post('/users/authenticate', globalBruteforce.prevent);
 app.post('/users', globalBruteforce.prevent);
 
+/*
+var corsOptions = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '/holaa/com');
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+};
+*/
+
+app.use(cors(corsOptions));
+
 // use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -47,19 +60,10 @@ app.use(morgan('dev'));
 // Use the passport package in our application
 app.use(passport.initialize());
 
-/*
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
-    next();
-});
-*/
-app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
+app.use('/', routes);
 
-app.use('/', cors(corsOptions), routes);
+app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
