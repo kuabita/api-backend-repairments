a754fc4dd69627993feb6c512e2100bfb6692d2a
Jose Giraldo <Jose.X.Giraldo.-ND@disney.com>
2017-04-12 10:03:54 -0500

    add logic to encrypt and decrypt password

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,22 +1,34 @@
 var User      = require('../models/user'),
 	jwt       = require('jwt-simple'),
 	config    = require('../config/database'),
-	jwtHelper = require('../helpers/jwt');
+	jwtHelper = require('../helpers/jwt'),
+	crypto    = require('crypto');
+
+function encryptPassword(email, pass) {
+    var hmac = crypto.createHmac('sha1', email).update(pass).digest('hex');
+    return hmac;
+}
 
 exports.createUser = function(req, res) {
-	var user = new User({ 
-    	email: req.body.email,
-    	password: req.body.password,
-    	rol: req.body.rol,
-  	});
+	var encryptedPass = encryptPassword(req.body.email, req.body.password)
 
-  	// save the User
-    user.save(function(err) {
-    	if (err) throw err;
+    User.findOne({email:req.body.email},function(err, user){
+        if(!user) {
+            var user = new User({ 
+		    	email: req.body.email,
+		    	password: encryptedPass,
+		    	role: req.body.rol,
+	  		});
+	        
+		    user.save(function(err) {
+		    	if (err) throw err;
 
-    	console.log('User saved successfully');
-    	res.json(user);
-    });
+		    	res.json(user);
+		    });
+      	} else {
+        	res.json('ya existe');
+        }
+    })
 };
 
 exports.getAllUsers = function(req, res, next) {
@@ -37,30 +49,20 @@ exports.getAllUsers = function(req, res, next) {
 };
 
 exports.authenticateUser = function(req, res) {
-    // find the user
-    User.findOne({
-    	email: req.body.email
-  	}, function(err, user) {
+    User.findOne({email: req.body.email}, function(err, user) {
 	    if (err) throw err;
 
 	    if (!user) {
 	        res.json({ success: false, message: 'Authentication failed. User not found.' });
 	    } else if (user) {
-	    	// check if password matches
-	        //user.comparePassword(req.body.password, function (err, isMatch) {
-		      //  if (isMatch && !err) {
+	    	var encryptedPass = encryptPassword(req.body.email, req.body.password)
 
-
-		      	// check if password matches
-			    if (user.password === req.body.password) {
-			        // if user is found and password is right create a token
-		            var token = jwt.encode(user, config.secret);
-		            // return the information including token as JSON
-		            res.json({success: true, token: 'JWT ' + token});
-		        } else {
-		            res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-		        }
-	      //  });
+	      	if(user.password === encryptedPass) {
+	            var token = jwt.encode(user, config.secret);
+	            res.json({success: true, token: 'JWT ' + token});
+	        } else {
+	            res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+	        }
 	    }
     });
 };
